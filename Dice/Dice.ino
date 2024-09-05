aa2aa31525bc2bd2f9fb7b590124131e38debde9
Azatrox <kellinwilhelm@aol.com>
2024-09-05 11:24:31 +0200

    Add files via upload

---
+++ Dice/Dice.ino
@@ -0,0 +1,209 @@
+int topright = 6;
+int topleft = 2;
+int middleright = 7;
+int middlecenter = 5;
+int middleleft = 3;
+int bottomright = 8;
+int bottomleft = 4;
+int button = 9;
+int i = 1;
+int b = 0;
+int a = 0;
+int c = 0;
+int lastroll = 0;
+
+void roll1()
+{
+  digitalWrite(middlecenter, HIGH);
+  lastroll = 1;
+}
+
+void roll2()
+{
+  digitalWrite(bottomright, HIGH);
+  digitalWrite(topleft, HIGH);
+   lastroll = 2;
+}
+
+void roll3()
+{
+  digitalWrite(middlecenter, HIGH);
+  digitalWrite(bottomright, HIGH);
+  digitalWrite(topleft, HIGH);
+  lastroll = 3;
+}
+
+void roll4()
+{
+  digitalWrite(bottomright, HIGH);
+  digitalWrite(bottomleft, HIGH);
+  digitalWrite(topright, HIGH);
+  digitalWrite(topleft, HIGH);
+  lastroll = 4;
+}
+
+void roll5()
+{
+  digitalWrite(bottomright, HIGH);
+  digitalWrite(bottomleft, HIGH);
+  digitalWrite(topright, HIGH);
+  digitalWrite(topleft, HIGH);
+  digitalWrite(middlecenter, HIGH);
+  lastroll = 5;
+}
+
+void roll6()
+{
+  digitalWrite(bottomright, HIGH);
+  digitalWrite(bottomleft, HIGH);
+  digitalWrite(topright, HIGH);
+  digitalWrite(topleft, HIGH);
+  digitalWrite(middleright, HIGH);
+  digitalWrite(middleleft, HIGH);
+  lastroll = 6;
+}
+
+void off()
+{
+  digitalWrite(middlecenter, LOW);  
+  digitalWrite(bottomright, LOW);
+  digitalWrite(bottomleft, LOW);
+  digitalWrite(topright, LOW);
+  digitalWrite(topleft, LOW);
+  digitalWrite(middleright, LOW);
+  digitalWrite(middleleft, LOW);
+}
+
+void on()
+{
+  digitalWrite(middlecenter, HIGH);  
+  digitalWrite(bottomright, HIGH);
+  digitalWrite(bottomleft, HIGH);
+  digitalWrite(topright, HIGH);
+  digitalWrite(topleft, HIGH);
+  digitalWrite(middleright, HIGH);
+  digitalWrite(middleleft, HIGH);
+}
+
+void actualroll() {
+  int roll = random(1, 7);
+  switch (roll) {
+    case 1:
+      roll1();
+      Serial.println(lastroll);
+      delay(i);
+      break;
+    case 2:
+      roll2();
+      Serial.println(lastroll);
+      delay(i);
+      break;
+    case 3:
+      roll3();
+      Serial.println(lastroll);
+      delay(i);
+      break;
+    case 4:
+      roll4();
+      Serial.println(lastroll);
+      delay(i);
+      break;
+    case 5:
+      roll5();
+      Serial.println(lastroll);
+      delay(i);
+      break;
+    case 6:
+      roll6();
+      Serial.println(lastroll);
+      delay(i);
+      break;
+  }
+  delay(i);
+  off();
+  delay(i);
+}
+
+
+void setup()
+{
+  pinMode(topright, OUTPUT);
+  pinMode(middleright, OUTPUT);
+  pinMode(bottomright, OUTPUT);
+  pinMode(middlecenter, OUTPUT);
+  pinMode(topleft, OUTPUT);
+  pinMode(middleleft, OUTPUT);
+  pinMode(bottomleft, OUTPUT);
+  pinMode(button, INPUT);
+  Serial.begin(9600);
+  randomSeed(analogRead(0));
+}
+
+
+void loop()
+{
+  while(c == 0)
+  {
+    {    
+      if (b == 1)
+      {
+        Serial.println("rolling now");
+        Serial.println(i);
+        actualroll();
+        if (i < 1015)
+        {
+          i+= 15;
+        }    
+        if (i > 1000)
+        {
+          c = 1;
+          break;
+        }    
+      } 
+      if (digitalRead(button)== HIGH)
+      {
+        actualroll();
+        a = 1;
+      }
+      if (digitalRead(button)== LOW && a == 1)
+      {
+        b = 1;
+      }
+    }
+  }
+  last();
+}
+
+
+void last()
+{
+  Serial.println("last called");
+  if (c == 1)
+  {
+    if (lastroll == 1)
+    {
+      roll1();
+    }
+    if (lastroll == 2)
+    {
+      roll2();
+    }
+    if (lastroll == 3)
+    {
+      roll3();
+    }
+    if (lastroll == 4)
+    {
+      roll4();
+    }
+    if (lastroll == 5)
+    {
+      roll5();
+    }
+    if (lastroll == 6)
+    {
+      roll6();
+    }
+  }
+}
+    
